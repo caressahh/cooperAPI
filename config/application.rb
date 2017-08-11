9fcc4752b67300653481a9217835454119586d80
Carissa <carissa@edgren.nu>
2017-08-11 15:44:19 +0200

    Try to fix CORS

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module CooperApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete]
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+                  expose: %w(access-token expiry token-type uid client),
+                        max_age: 0
       end
     end
   end
